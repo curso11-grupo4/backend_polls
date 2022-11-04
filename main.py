04361793b3ef7a8fce8e25a300e41acd14eb72a5
Alejandro Velasco <alejandrovelasco094@gmail.com>
2022-11-04 13:25:26 -0500

    emular clase

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import json
 
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, jsonify
+from flask_cors import CORS, cross_origin
 from waitress import serve
+
 
 app = Flask(__name__)
 cors = CORS(app)
+
+
+@app.route("/",methods=['GET'])
+def test():
+    responder = {"message": "Server running ..."}
+    return jsonify(responder)
 
 
 def load_file_config():
@@ -16,5 +23,5 @@ def load_file_config():
 
 if __name__ == '__main__':
     data_config = load_file_config()
-    print("Server running: https://" + data_config.get('url-backend') + ":" + str(data_config.get('port')))
+    print("Server running: http://" + data_config.get('url-backend') + ":" + str(data_config.get('port')))
     serve(app, host=data_config.get('url-backend'), port=data_config.get('port'))
