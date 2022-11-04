4c75d8a10de73f5e0481a948a816cd52ec58e5a3
AndresDuqueH <andresfdh@gmail.com>
2022-11-04 15:12:42 -0500

    repositories

--- repositories/interfaceRepository.py
+++ repositories/interfaceRepository.py
@@ -51,7 +51,7 @@ class InterfaceRepository(Generic[T]):
             document = {}
         return document
 
-    def save(self, item: T) -> T:
+    def save(self, item: T) -> dict:
         current_collection = self.data_base[self.collection]
         item = self.transform_refs(item)
         if hasattr(item, '_id') and item._id != "":
@@ -59,25 +59,50 @@ class InterfaceRepository(Generic[T]):
             id_ = item._id
             _id = ObjectId(id_)
             delattr(item, '_id')
-            item = item.__dict__
-            update_item = {"$set", item}
+            item_dict = item.__dict__
+            update_item = {"$set", item_dict}
             current_collection.update_one({'_id': _id}, update_item)
         else:
             # insert
             _id = current_collection.insert_one(item.__dict__)
+            id_ = _id.inserted_id.__str__()
+        return self.find_by_id(id_)
 
     # TODO Verificar el tipo de dato a retornar
     def update(self, id_: str, item: T) -> dict:
-        pass
+        current_collection = self.data_base[self.collection]
+        _id = ObjectId(id_)
+        item_dict = item.__dict__
+        updated_item = {"$set": item_dict}
+        result = current_collection.update_one({'_id': _id}, updated_item)
+        return {"update_count": result.matched_count}
+
 
     def delete(self, id_: str) -> dict:
-        pass
+        current_collection = self.data_base[self.collection]
+        _id = ObjectId(id_)
+        result = current_collection.delete_one({'_id': _id})
+        return {"delete_count": result.deleted_count}
 
     def query(self, query: dict) -> list:
-        pass
+        current_collection = self.data_base[self.collection]
+        dataset = []
+        for document in current_collection.find(query):
+            document['_id'] = document['_id'].__str__()
+            document = self.transform_obj_ids(document)
+            document = self.get_values_db_ref(document)
+            dataset.append(document)
+        return dataset
 
     def query_aggregation(self, query: dict) -> list:
-        pass
+        current_collection = self.data_base[self.collection]
+        dataset = []
+        for document in current_collection.aggregate(query):
+            document['_id'] = document['_id'].__str__()
+            document = self.transform_obj_ids(document)
+            document = self.get_values_db_ref(document)
+            dataset.append(document)
+        return dataset
 
     # Sprint1
     def get_values_db_ref(self, document) -> T:
@@ -89,11 +114,10 @@ class InterfaceRepository(Generic[T]):
     def transform_obj_ids(self):
         pass
 
-    def transform_refs(self):
-        pass
-    # Sprint 1
-
     def format_list(self):
+        pass
+
+    def transform_refs(self):
         pass
 
     def object_to_db_ref(self):
---
+++ repositories/table_repository.py
@@ -0,0 +1,6 @@
+from models.table import Table
+from repositories.interfaceRepository import InterfaceRepository
+
+
+class Table(InterfaceRepository[Table]):
+    pass
